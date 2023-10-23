78f34f8b2142771117c0ab4b677d5e2ca6d9fa7a
janielmarques <janielmarques@icloud.com>
2023-10-23 16:32:12 -0300

    Entrada de dados.

--- Principal.java
+++ Principal.java
@@ -1,12 +1,22 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         String x;
+        int y;
+        double z;
         x = sc.next();
-        System.out.println("você digitou: " + x);
+        y = sc.nextInt();
+        z = sc.nextDouble();
+        System.out.println("dados digitados:" );
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
 
 
 
