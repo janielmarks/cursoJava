024d00ef8db56dce380056811aed0d8231366ce7
janielmarques <janielmarques@icloud.com>
2023-10-23 16:08:51 -0300

    first commit

---
+++ Principal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String x;
+        x = sc.next();
+        System.out.println("você digitou: " + x);
+
+
+
+        sc.close();
+    }
+}
